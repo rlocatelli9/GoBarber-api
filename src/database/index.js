94d68968bef91b50dbf3787eec96af976c20c8c9
Robson Locatelli <rlocatelli9@gmail.com>
2020-02-09 19:08:16 -0300

    mongodb settings

--- src/database/index.js
+++ src/database/index.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import Sequelize from 'sequelize';
 import Appointment from '../app/models/Appointment';
 import File from '../app/models/File';
@@ -9,6 +10,7 @@ const models = [User, File, Appointment];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
@@ -17,6 +19,17 @@ class Database {
     models
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/gobarber',
+      {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+        useUnifiedTopology: true,
+      }
+    );
   }
 }
 
