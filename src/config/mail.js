5c535f8450042d3f597ef9ae1873d44cd27d52c9
Robson Locatelli <rlocatelli9@gmail.com>
2020-03-27 00:28:49 -0300

    fix: changed variable

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,7 +1,7 @@
 export default {
   host: process.env.MAIL_HOST,
   port: process.env.MAIL_PORT,
-  secure: process.env.MAIL_SECURE, // true for 465, false for other ports
+  secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
