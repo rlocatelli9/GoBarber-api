1c0ac7d1c0698ccecbac53a14485a5c032ce2091
Robson Locatelli <rlocatelli9@gmail.com>
2020-03-27 00:23:55 -0300

    feat: added cors

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import express from 'express';
 import 'express-async-errors';
 import { resolve } from 'path';
+import cors from 'cors';
 import Youch from 'youch';
 import sentryConfig from './config/sentry';
 import './database';
@@ -21,6 +22,7 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
