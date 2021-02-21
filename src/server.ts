c00d62b41a6e727fc3bcc6d810ee0b0c56e2c92a
Robson Locatelli <robson.locatelli.m@gmail.com>
2021-02-21 09:26:12 -0300

    feat: enable CORS

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import 'reflect-metadata';
+import cors from 'cors';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
@@ -12,6 +13,7 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
