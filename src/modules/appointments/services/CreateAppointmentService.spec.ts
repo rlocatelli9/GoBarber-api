fa428ab4c9bbebd365f0751fb2cf0db5940467a8
Robson Locatelli <robson.locatelli.m@gmail.com>
2021-05-12 19:09:04 -0300

    feat: test two - should not be able to create two appointments on the same time

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,5 @@
+import AppError from '@shared/errors/AppError';
+
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -17,7 +19,24 @@ describe('CreateAppointment', () => {
     expect(appointment.providerId).toBe('21321321321');
   });
 
-  // it('should not be able to create two appointments on the same time', () => {
-  //   expect(1 + 2).toBe(3);
-  // });
+  it('should not be able to create two appointments on the same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointmentDate = new Date(2020, 4, 10, 18);
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      providerId: '21321321321',
+    });
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        providerId: '21321321321',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
