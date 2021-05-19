31bbc93ae349fa3b9bc4f78229ec2a0c3584d3d2
Robson Locatelli <robson.locatelli.m@gmail.com>
2021-05-19 15:09:03 -0300

    Express rate limit

--- src/config/cache.ts
+++ src/config/cache.ts
@@ -11,9 +11,9 @@ export default {
   driver: 'redis',
   config: {
     redis: {
-      host: 'localhost',
-      port: 6379,
-      password: undefined,
+      host: process.env.REDIS_HOST,
+      port: Number(process.env.REDIS_PORT),
+      password: process.env.REDIS_PASSWORD || undefined,
     },
   },
 } as ICacheConfig;
