97f1b04d21ae9b3dfad6e074577603766c25b1ec
Robson Locatelli <robson.locatelli.m@gmail.com>
2021-05-19 12:04:13 -0300

    invalidando cache

--- src/shared/container/providers/CacheProvider/models/interfaces/CacheProvider.ts
+++ src/shared/container/providers/CacheProvider/models/interfaces/CacheProvider.ts
@@ -2,4 +2,5 @@ export default interface ICacheProvider {
   save(key: string, value: unknown): Promise<void>;
   recover<T>(key: string): Promise<T | null>;
   invalidate(key: string): Promise<void>;
+  invalidatePrefix(prefix: string): Promise<void>;
 }
