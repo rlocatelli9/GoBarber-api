675608609db64d8b906eb13e8fc46cac0f2e3df1
Robson Locatelli <robson.locatelli.m@gmail.com>
2021-05-23 12:32:59 -0300

    fix: serializando cache e cliente dos agendamentos

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -66,6 +66,7 @@ class AppointmentsRepository implements IAppointmentsRepository {
             `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
         ),
       },
+      relations: ['user'],
     });
 
     return appointments;
