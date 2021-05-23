ddec95d4d76fb5064e6112e2e0183b1463f04a77
Robson Locatelli <robson.locatelli.m@gmail.com>
2021-05-23 11:21:06 -0300

    fix: mostrando false para lista que contem data no passado

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,5 +1,5 @@
 import { inject, injectable } from 'tsyringe';
-import { getDate, getDaysInMonth } from 'date-fns';
+import { getDate, getDaysInMonth, isAfter } from 'date-fns';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/interfaces/AppointmentsRepository';
 
@@ -42,13 +42,16 @@ class ListProviderMonthAvailabilityService {
     );
 
     const availability = eachDayOfMonth.map(day => {
+      const compareDate = new Date(year, month - 1, day, 23, 59, 59);
+
       const appointmentsInDay = appointments.filter(appointment => {
         return getDate(appointment.date) === day;
       });
 
       return {
         day,
-        available: appointmentsInDay.length < 10,
+        available:
+          isAfter(compareDate, new Date()) && appointmentsInDay.length < 10,
       };
     });
 
